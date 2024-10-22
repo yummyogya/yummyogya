c18bdd0b439efcfec101612e421bf563b7e037e1
kiyyawww <woydoy18@gmail.com>
2024-10-22 16:24:46 +0700

    routing url wishlit 2

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from wishlist.models import Product  # Sesuaikan dengan nama app yang memuat model Product
 
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
