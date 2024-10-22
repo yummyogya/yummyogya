a2776395179593f485e725f9b27f18e03c8f56dd
kiyyawww <woydoy18@gmail.com>
2024-10-22 16:37:32 +0700

    wishlist issues 2

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from wishlist.models import Product  
+from yummyogya.models import Product  
 
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Wishlist
-from wishlist.models import Product  
+from yummyogya.models import Product  
 
 @login_required
 def add_to_wishlist(request, product_id):
