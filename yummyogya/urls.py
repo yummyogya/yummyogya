7f20fb1ffc4422f811ef360af822ceb8a5816025
farrellzidane <raihandrawan@gmail.com>
2024-10-22 11:38:28 +0700

    #8 Editted navbar and urls.py

--- yummyogya/urls.py
+++ yummyogya/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('authentication/', include('authentication.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ]
