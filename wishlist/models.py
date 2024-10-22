d78e35cc5a0babd01be2fc701fbb57574762176d
kiyyawww <woydoy18@gmail.com>
2024-10-22 16:53:07 +0700

    progress wishlist

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from wishlist.models import Makanan  
+from django.apps import apps  
 
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Makanan, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        'wishlist.Makanan', on_delete=models.CASCADE  
+    )
 
     def __str__(self):
         return f"{self.user.username} - {self.product.name}"
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.apps import apps
 from .models import Wishlist
-from wishlist.models import Makanan  
 
 @login_required
 def add_to_wishlist(request, product_id):
+    Makanan = apps.get_model('wishlist', 'Makanan')  
     product = get_object_or_404(Makanan, id=product_id)
     Wishlist.objects.get_or_create(user=request.user, product=product)
-    return redirect('wishlist')  # Arahkan ke halaman wishlist setelah ditambah
+    return redirect('wishlist')  
 
 @login_required
 def wishlist_view(request):
@@ -16,8 +17,9 @@ def wishlist_view(request):
 
 @login_required
 def remove_from_wishlist(request, product_id):
+    Makanan = apps.get_model('wishlist', 'Makanan')  
     product = get_object_or_404(Makanan, id=product_id)
     wishlist_item = Wishlist.objects.filter(user=request.user, product=product)
     if wishlist_item.exists():
         wishlist_item.delete()
-    return redirect('wishlist')  # Arahkan ke halaman wishlist setelah dihapus
+    return redirect('wishlist')  
