67b851e0ffe39c19b33e78fc58f3dd827f6b398c
farrellzidane <raihandrawan@gmail.com>
2024-10-22 10:41:00 +0700

    edit navbar

--- yummyogya/urls.py
+++ yummyogya/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('', include('dashboard.urls')),
     path('authentication/', include('authentication.urls')),
 ]
