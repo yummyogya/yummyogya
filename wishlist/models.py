c3366139b4f63529f23f363d6deb3c29fb6b8dfa
kiyyawww <woydoy18@gmail.com>
2024-10-27 21:08:54 +0700

    resolve tampilan wishlist.html

--- wishlist/models.py
+++ wishlist/models.py
@@ -2,18 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 from main.models import Makanan
 
-# class Food(models.Model):
-#     name = models.CharField(max_length=100)
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-#     description = models.TextField()
-#     category = models.CharField(max_length=50)
-#     restaurant = models.CharField(max_length=100)
-#     rating = models.FloatField()
-#     image_url = models.URLField()
-
-#     def __str__(self):
-#         return self.name
-
 class Wishlist(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     food = models.ManyToManyField(Makanan, related_name='wishlists')
