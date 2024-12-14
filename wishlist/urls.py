0cb34579aa4acb0d061f889d0d8d4cb844d6f655
kiyyawww <woydoy18@gmail.com>
2024-12-14 13:50:30 +0700

    get wishlist json

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,6 +6,6 @@ app_name = 'wishlist'
 urlpatterns = [
     path('add/<int:food_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/', views.view_wishlist, name='view_wishlist'),
-    path('wishlist/json/', views.get_wishlist_json, name='wishlist_json'),
+    path('wishlist/json/', views.get_wishlist_json, name='wishlist_json'), # url get json
     path('wishlist/remove-from-wishlist/<int:food_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
