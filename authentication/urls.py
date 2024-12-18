66935407a17134a42618a1b9245c6ada40035ed2
Wh0Rv <salsabilacf04@icloud.com>
2024-12-19 00:54:02 +0700

    fix conflict

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("register/", register, name="register"),
     path("login/", login_user, name="login"),
     path('logout/', logout_user, name='logout'),
+    path('register_flutter/', register_flutter, name='register_flutter'),
+    path('login_flutter/', login_flutter, name='login_flutter'),
     path('logout_flutter/', logout_flutter, name='logout_flutter'),
 ]
