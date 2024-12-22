6334f86b14ae427019b63a6102b45d21441560c2
aliyahnhsa <aliyahnahisa@gmail.com>
2024-12-22 13:25:33 +0700

    update details flutter

--- details/urls.py
+++ details/urls.py
@@ -1,10 +1,12 @@
 # details/urls.py
 from django.urls import path
+
 from . import views
 
 app_name = 'details'
 
 urlpatterns = [
     path('details/<int:id>/', views.food_detail, name='food_detail'),
+    path('details_flutter/', views.food_detail_flutter_query, name='food_detail_flutter_query'),
     path('add_review/', views.add_review, name='add_review'),
 ]
--- details/views.py
+++ details/views.py
@@ -17,7 +17,7 @@ def add_review(request):
         rating = data.get('rating')
         review_text = data.get('review')
         food_id = data.get('food_id')
-        
+
         # Ambil objek food dan simpan ulasan
         food = Makanan.objects.get(id=food_id) if food_id else None
         if food and request.user.is_authenticated:
@@ -33,10 +33,39 @@ def add_review(request):
             average_rating = all_reviews.aggregate(models.Avg('rating'))['rating__avg']
             food.rating = average_rating  # Update rating di objek food
             food.save()  # Simpan perubahan ke database
-            
+
             return JsonResponse({'message': 'Review added successfully!', 'status': 'success'})
         return JsonResponse({'message': 'Failed to add review', 'status': 'error'})
     return JsonResponse({'message': 'Invalid request method', 'status': 'error'})
+
+@csrf_exempt
+def food_detail_flutter_query(request):
+    if request.method == "GET":
+        food_id = request.GET.get('food_id')
+        if not food_id:
+            return JsonResponse({"status": "error", "message": "food_id is required"}, status=400)
+
+        try:
+            food = Food.objects.get(id=food_id)
+            reviews = Review.objects.filter(food_alt=food).values(
+                "user__username", "rating", "review", "created_at"
+            )  # Ambil semua review terkait dengan `food`
+
+            data = {
+                "id": food.id,
+                "name": food.name,
+                "description": food.description,
+                "price": food.price,
+                "rating": food.rating,
+                "restaurant": food.restaurant,
+                "image_url": food.image_url,
+                "reviews": list(reviews),  # Tambahkan daftar review ke dalam respons
+            }
+            return JsonResponse({"status": "success", "data": data}, status=200)
+        except Food.DoesNotExist:
+            return JsonResponse({"status": "error", "message": "Makanan tidak ditemukan"}, status=404)
+
+    return JsonResponse({"status": "error", "message": "Method not allowed"}, status=405)
 
 def food_detail(request, id):
     item = Makanan.objects.filter(pk=id).first() or Food.objects.filter(pk=id).first()
